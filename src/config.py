c726ef9fc07c10e72bec17a6bb757c9f1a1de426
franckoline@hotmail.com <franckoline@hotmail.com>
2025-05-12 23:46:32 +0100

    turn off the jwt verify sub

--- src/config.py
+++ src/config.py
@@ -12,4 +12,5 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     JWT_ACCESS_TOKEN_EXPIRES = 3600  # 1 hour
+    JWT_VERIFY_SUB = False
 
